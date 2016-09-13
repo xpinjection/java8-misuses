1fb0bce89aa44e71c211c5261e5920e76e5b43a7
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2016-09-13 12:07:51 +0300

    added one more sample about stream API misuse

---
+++ src/com/xpinjection/java8/misused/stream/WantToUseStreamsEverywhere.java
@@ -0,0 +1,49 @@
+// ============================================================================
+//  File          : WantToUseStreamsEverywhere
+//  Created       : 13.09.2016   
+//  Description   :
+//  Modifications :
+//
+// ============================================================================
+//  Copyright(c) 2016 XP Injection, Ukraine
+// ============================================================================
+package com.xpinjection.java8.misused.stream;
+
+import com.xpinjection.java8.misused.Annotations.Good;
+import com.xpinjection.java8.misused.Annotations.Ugly;
+
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+/**
+ * @author Alimenkou Mikalai
+ * @version 1.0
+ */
+public class WantToUseStreamsEverywhere {
+    @Ugly
+    class UseStreamToBuildMap {
+        public Map<String, Object> getJpaProperties() {
+            return Collections.unmodifiableMap(
+                    Stream.of(
+                            new AbstractMap.SimpleEntry<>("hibernate.show_sql", "true"),
+                            new AbstractMap.SimpleEntry<>("hibernate.format_sql", "true")
+                    ).collect(toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue))
+            );
+        }
+    }
+
+    @Good
+    class UseOldPlainMap {
+        public Map<String, Object> getJpaProperties() {
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("hibernate.show_sql", "true");
+            properties.put("hibernate.format_sql", "true");
+            return Collections.unmodifiableMap(properties);
+        }
+    }
+}
