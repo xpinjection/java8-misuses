32b85f4fef3c1df1e1a24fff459f6a75742b0911
Sergey Chupov <scadgek@live.com>
2016-10-18 23:37:06 +0300

    Add another good sample of map transformation. (#5)
    Creating a map with "new" keyword and then populating it using forEach is still an imperative approach. The proposed one is more declarative and performs transformation on the original Map.

--- src/com/xpinjection/java8/misused/lambda/collections/MapIterating.java
+++ src/com/xpinjection/java8/misused/lambda/collections/MapIterating.java
@@ -1,11 +1,12 @@
 package com.xpinjection.java8.misused.lambda.collections;
 
-import com.xpinjection.java8.misused.User;
 import com.xpinjection.java8.misused.Annotations.Good;
 import com.xpinjection.java8.misused.Annotations.Ugly;
+import com.xpinjection.java8.misused.User;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MapIterating {
     @Ugly
@@ -24,6 +25,17 @@ public class MapIterating {
             Map<String, String> userNames = new HashMap<>();
             users.forEach((key, value) -> userNames.put(key, value.getName()));
             return userNames;
+        }
+    }
+
+    @Good
+    class UsingMapTransform {
+        public Map<String, String> getUserNames(Map<String, User> users) {
+            return users.entrySet().stream()
+                    .collect(
+                            Collectors.toMap(
+                                    Map.Entry::getKey,
+                                    entry -> entry.getValue().getName()));
         }
     }
 }
