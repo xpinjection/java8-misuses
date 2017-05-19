5c7d4ab1b4813c58ec353fd7736c57317386292e
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-05-20 01:24:02 +0300

    added some intermediate ugly options for matching element

--- src/com/xpinjection/java8/misused/stream/MatchElementInFunctionalStyle.java
+++ src/com/xpinjection/java8/misused/stream/MatchElementInFunctionalStyle.java
@@ -9,7 +9,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ExternalStateWithForEach {
+public class MatchElementInFunctionalStyle {
     private final Set<User> users = new HashSet<>();
 
     @Ugly
@@ -26,6 +26,26 @@ public class ExternalStateWithForEach {
                     )
             );
             return found.get();
+        }
+    }
+
+    @Ugly
+    class TryToUseFunctionalStyleWithStreamFilter {
+        public boolean checkPermission(Permission permission) {
+            return users.stream().filter(
+                    u -> u.getRoles().stream()
+                            .filter(r -> r.getPermissions().contains(permission))
+                            .count() > 0)
+                    .findFirst().isPresent();
+        }
+    }
+
+    @Ugly
+    class TryToUseStreamMatching {
+        public boolean checkPermission(Permission permission) {
+            return users.stream()
+                    .anyMatch(u -> u.getRoles().stream()
+                            .anyMatch(r -> r.getPermissions().contains(permission)));
         }
     }
 
