db68da6840a108cdfca43872a8b8f18d3b3c6a94
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-05-19 17:46:50 +0300

    added example with "clever" Optional map play

---
+++ src/com/xpinjection/java8/misused/optional/IfStatementIsNotAlwaysBadThing.java
@@ -0,0 +1,38 @@
+package com.xpinjection.java8.misused.optional;
+
+import com.xpinjection.java8.misused.Annotations.Good;
+import com.xpinjection.java8.misused.Annotations.Ugly;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class IfStatementIsNotAlwaysBadThing {
+    @Ugly
+    class CombineSomeOptionalsInCleverWay {
+        Optional<Integer> sum(Optional<Integer> first, Optional<Integer> second) {
+            return Stream.of(first, second)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .reduce(Integer::sum);
+        }
+    }
+
+    @Ugly
+    class PlayMapGameInEvenMoreCleverWay {
+        Optional<Integer> sum(Optional<Integer> first, Optional<Integer> second) {
+            return first.map(b -> second.map(a -> b + a).orElse(b))
+                    .map(Optional::of)
+                    .orElse(second);
+        }
+    }
+
+    @Good
+    class OldSchoolButTotallyClearCode {
+        Optional<Integer> sum(Optional<Integer> first, Optional<Integer> second) {
+            if (!first.isPresent() && !second.isPresent()) {
+                return Optional.empty();
+            }
+            return Optional.of(first.orElse(0) + second.orElse(0));
+        }
+    }
+}
