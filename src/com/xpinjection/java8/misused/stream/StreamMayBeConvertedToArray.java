dfe5158d49f6a9b6cad43bc6ef88edece1bbc6f1
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-05-21 00:25:34 +0300

    added sample on array conversion

---
+++ src/com/xpinjection/java8/misused/stream/StreamMayBeConvertedToArray.java
@@ -0,0 +1,29 @@
+package com.xpinjection.java8.misused.stream;
+
+import com.xpinjection.java8.misused.Annotations.Good;
+import com.xpinjection.java8.misused.Annotations.Ugly;
+import com.xpinjection.java8.misused.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMayBeConvertedToArray {
+    @Ugly
+    class ConvertToArrayViaList {
+        public String[] getUserNames(List<User> users) {
+            List<String> names = users.stream()
+                    .map(User::getName)
+                    .collect(Collectors.toList());
+            return names.toArray(new String[names.size()]);
+        }
+    }
+
+    @Good
+    class ConvertToArrayDirectly {
+        public String[] getUserNames(List<User> users) {
+            return users.stream()
+                    .map(User::getName)
+                    .toArray(String[]::new);
+        }
+    }
+}
