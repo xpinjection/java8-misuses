fe05df31d7da398ac77e1f05e0c4d1f24529df97
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-08-25 14:02:17 +0300

    added sample for suitable internal data structure

--- src/com/xpinjection/java8/misused/stream/DoNotNeglectDataStructures.java
+++ src/com/xpinjection/java8/misused/stream/DoNotNeglectDataStructures.java
@@ -3,8 +3,7 @@ package com.xpinjection.java8.misused.stream;
 import com.xpinjection.java8.misused.Annotations.Good;
 import com.xpinjection.java8.misused.Annotations.Ugly;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static java.util.stream.Collectors.toList;
 
@@ -14,8 +13,7 @@ public class DoNotNeglectDataStructures {
         public List<Order> filterOrdersByStatuses(List<Order> orders, Set<Status> appropriateStatuses) {
             return orders.stream()
                     .filter(order ->
-                            appropriateStatuses.stream().anyMatch(status ->
-                                    status.equals(order.getStatus())))
+                            appropriateStatuses.stream().anyMatch(order.getStatus()::equals))
                     .collect(toList());
         }
     }
@@ -29,12 +27,48 @@ public class DoNotNeglectDataStructures {
         }
     }
 
-    class Order {
-        Status getStatus() {
-            return null; //stub
+    @Ugly
+    class StateIsStoredInBadDataStructure {
+        private final List<Order> orders = new ArrayList<>();
+
+        public void placeOrder(Order order) {
+            orders.add(order);
+        }
+
+        public List<Order> getOrdersInStatus(Status status) {
+            return orders.stream()
+                    .filter(order -> order.getStatus() == status)
+                    .collect(toList());
         }
     }
 
-    class Status {
+    @Good
+    class InternalDataStructureMayBeOptimizedForAccessMethods {
+        //Use multimap instead from external collections like Guava
+        private final Map<Status, List<Order>> orders = new EnumMap<>(Status.class);
+
+        public void placeOrder(Order order) {
+            orders.computeIfAbsent(order.getStatus(), status -> new ArrayList<>()).add(order);
+        }
+
+        public List<Order> getOrdersInStatus(Status status) {
+            return orders.get(status);
+        }
+    }
+
+    class Order {
+        private Status status = Status.ACTIVE;
+
+        Status getStatus() {
+            return status;
+        }
+
+        void setStatus(Status status) {
+            this.status = status;
+        }
+    }
+
+    enum Status {
+        ACTIVE, SUSPENDED, CLOSED
     }
 }
